846e6a1ff002a72655f11be46f4953276625258e
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-02-13 19:55:01 -0800

    returns word_list

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -21,24 +21,29 @@ def count_words(subreddit, word_list, hot_list=[], after=""):
         hot_list = [dd for dd in hot_list if dd.get('occur') > 0]
         if not hot_list:
             return None
+            return word_list
         else:
             hot_list = sorted(hot_list, key=lambda k: k['occur'], reverse=True)
             for h in hot_list:
                 print("{}: {}".format(h.get('word'), h.get('occur')))
+            # hot_list2 = [(h.get('word'), h.get('occur')) for h in hot_list]
+            # return hot_list2
+            return word_list
     else:
         res = response.json().get('data').get('children')
         t_list = [d.get('data').get('title') for d in res]
-        t_list = [word.lower() for item in t_list for word in item.split()]
+        t_list = [word.lower() for item in t_list for word in item.split(" ")]
         t_list.sort()
-        temp_list = []
         for wd in hot_list:
             wd['occur'] = wd.get('occur') +\
                           t_list.count(wd.get('word').lower())
-        # print ("++++++++++++++++++++++++++++")
-        # print (t_list)
-        # for h in hot_list:
-        #     print("{}: {}".format(h.get('word'), h.get('occur')))
-        # print("after {}".format(after))
-        # print("+++++++++++++++++++++++++++++")
+        """
+        print ("++++++++++++++++++++++++++++")
+        print (t_list)
+        for h in hot_list:
+            print("{}: {}".format(h.get('word'), h.get('occur')))
+        print("after {}".format(after))
+        print("+++++++++++++++++++++++++++++")
+        """
         after = response.json().get('data').get('after')
         return(count_words(subreddit, word_list, hot_list, after))
