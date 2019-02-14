31e3be300bbc787ed4eee55558e8fa28e607d591
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-02-14 07:47:26 -0800

    removed duplicates

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -15,6 +15,7 @@ def count_words(subreddit, word_list, hot_list=[], after=""):
     else:
         response = requests.get(uri, allow_redirects=False,
                                 headers=headers, params=parameters)
+    word_list = set(word_list)
     if not hot_list:
         hot_list = [{'word': w, 'occur': 0} for w in word_list]
     if response.status_code != 200 or after is None:
