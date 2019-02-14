82c8c5c2808b60b4bc103610f5cfd7df63e69ec6
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-02-13 20:47:15 -0800

    changed return code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,7 @@ def number_of_subscribers(subreddit):
     user_agent = {'User-Agent': 'ubuntu:test.petehwu:v0.0.1 (by/u/petehwu)'}
     uri = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(uri, allow_redirects=False, headers=user_agent)
+    # print (response.status_code)
     if response.status_code != 200:
         return 0
     else:
